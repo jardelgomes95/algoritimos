77a2ece0599580bd35acb5d59719a219ba57fe93
jardelgomes95 <jardel.gomes95@hotmail.com>
2021-09-15 20:43:29 -0300

    Arrumando exercicio 2.6

--- exercicio2.6.py
+++ exercicio2.6.py
@@ -7,15 +7,14 @@ lado_C = float(input("Insira um valor C: "))
 
 #É um triangulo? 
 
-if (lado_A + lado_B > lado_C) and (lado_B + lado_C > lado_A) and (lado_C + lado_A > lado_B):
-    triangulo = True
+if ((lado_A + lado_B) > lado_C) and ((lado_B + lado_C) > lado_A) and ((lado_C + lado_A) > lado_B):
+    if (lado_A == (lado_B and lado_C)):
+        print("É um triângulo equilátero")
+    elif lado_A == (lado_B or lado_C) or lado_B == (lado_A or lado_C) or lado_C == (lado_A or lado_B):
+        print("É um triângulo Isósceles")
+    else:
+        print("É um triângulo Escaleno")
 else:
     triangulo = False
     print("Não é um triângulo")
 
-if ((triangulo == True) and (lado_A == (lado_B and lado_C))):
-    print("É um triângulo equilátero")
-elif (triangulo == True) and ((lado_A == (lado_B or lado_C)) or (lado_B == (lado_A or lado_C)) or (lado_C == (lado_A or lado_B))):
-    print("É um triângulo Isósceles")
-else:
-    print("É um triângulo Escaleno")
