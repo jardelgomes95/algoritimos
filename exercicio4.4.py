c9203d2bb77835809ba4f52b59dd181f73e0e614
jardelgomes95 <jardel.gomes95@hotmail.com>
2021-10-06 19:49:51 -0300

    consertando o exercicio 4.4

--- exercicio4.4.py
+++ exercicio4.4.py
@@ -2,5 +2,13 @@
 num_1 = int(input("Digite um número: "))
 num_2 = int(input("Digite um número: "))
 
-for n in range((num_1 + 1), num_2):
-    print(n)
+if num_1 < num_2:
+    for n in range((num_1 + 1), num_2):
+        print(n)
+
+elif num_2 < num_1:
+    for n in range((num_1 - 1), num_2, -1):
+        print(n)
+
+else:
+    print("Os números são iguais")
